5bdb6ce0701cede24b156cf02748d161b549949d
Themba Dube <embeddedthemba@gmail.com>
2020-10-06 17:55:26 -0400

    update to mathcore

---
+++ mathcore.js
@@ -0,0 +1,78 @@
+/* Math generation library */
+
+window.mathQuestions = [];
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function getCorrectAnswer(left, right) {
+    var currentCorrectAnswer;
+    if(window.mathOperation == "add") {
+        currentCorrectAnswer = left + right;
+    } else if(window.mathOperation == "subtract") {
+        currentCorrectAnswer = left - right;
+    } else if(window.mathOperation == "multiply") {
+        currentCorrectAnswer = left * right;
+    } else if(window.mathOperation == "divide") {
+        currentCorrectAnswer = left / right;
+    }
+    return currentCorrectAnswer;
+}
+
+window.generateMathQuestions = function(operation, maxResultSize) {
+    window.currentMathIndex = 0;
+    window.mathOperation = operation;
+    var leftMin = 0, leftMax = 0, rightMin = 0, rightMax = 0;
+    var isSubtractionLike = false;
+    if(operation == "add") {
+        window.mathSymbol = "&plus;";
+    } else if(operation == "subtract") {
+        window.mathSymbol = "&minus;";
+        isSubtractionLike = true;
+    } else if(operation == "multiply") {
+        window.mathSymbol = "&times;";
+    } else if(operation == "divide") {
+        window.mathSymbol = "&divide;";
+        isSubtractionLike = true;
+    }
+    leftMin = rightMin = 1;
+    leftMax = rightMax = (maxResultSize == 10 ? 9 : 5);
+    function isInt(value) {
+        return !isNaN(value) && 
+               parseInt(Number(value)) == value && 
+               !isNaN(parseInt(value, 10));
+    }
+    for(var left = leftMin; left <= leftMax; left++) {
+        for(var right = rightMin; right <= (isSubtractionLike ? left : rightMax); right++) {
+            if(isInt(getCorrectAnswer(left, right)))
+                window.mathQuestions.push([ left, right ]);
+            else {
+                console.log("Skipped: " + left + " " + right);
+            }
+        }
+    }
+    if(window.mathQuestions.length == 0)
+        throw new Error("No " + operation + " questions????");
+    console.log(window.mathQuestions.slice());
+    shuffle(window.mathQuestions);
+}
+
+window.getNextMathQuestion = function() {
+    if(window.currentMathIndex <= (window.mathQuestions.length - 1)) {
+        var operands = window.mathQuestions[window.currentMathIndex];
+        var currentCorrectAnswer = getCorrectAnswer(operands[0], operands[1]);
+        window.currentMathIndex++;
+        return { operands: operands, currentCorrectAnswer: currentCorrectAnswer };
+    } else {
+        shuffle(window.mathQuestions);
+        window.currentMathIndex = 0;
+        return window.getNextMathQuestion();
+    }
+}
--- script.js
+++ script.js
@@ -88,6 +88,8 @@ window.onchoosefn = function(event) {
     }
     window.maxResultSize = maxResultSize;
     
+    window.generateMathQuestions(operation, maxResultSize);
+    numCustomers = Math.min(Math.round(window.mathQuestions.length * 1.1), numCustomers);
     function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -167,34 +169,15 @@ window.onchoosefn = function(event) {
                 return;
             }
             var correctInitialColumn = getRandomIntInclusive(0, NUM_COLUMNS - 1);
-            currentCorrectAnswer = getRandomIntInclusive(1, maxResultSize);
+            /* MATH CORE BEGIN */
             var firstFactor, secondFactor, symbol;
+            var retrievedQuestion = window.getNextMathQuestion();
+            firstFactor = retrievedQuestion.operands[0];
+            secondFactor = retrievedQuestion.operands[1];
+            symbol = window.mathSymbol;
+            currentCorrectAnswer = retrievedQuestion.currentCorrectAnswer;
             
-            if(operation != null)
-                operation = operation.trim();
-            
-            if(operation == "add") {
-                symbol = "&plus;";
-                firstFactor = getRandomIntInclusive(1, currentCorrectAnswer);
-                secondFactor = currentCorrectAnswer - firstFactor;
-            } else if(operation == "subtract") {
-                symbol = "&minus;";
-                currentCorrectAnswer = getRandomIntInclusive(1, (window).maxResultSize);
-                secondFactor = getRandomIntInclusive(1, (window).maxResultSize);
-                firstFactor = currentCorrectAnswer + secondFactor;
-                console.log((window).maxResultSize);
-            } else if(operation == "multiply") {
-                symbol = "&times;";
-                firstFactor = getRandomIntInclusive(1, (window).maxResultSize);
-                secondFactor = getRandomIntInclusive(1, (window).maxResultSize);
-                currentCorrectAnswer = firstFactor * secondFactor;
-            } else if(operation == "divide") {
-                var divisor = getRandomIntInclusive(2, 6);
-                firstFactor = currentCorrectAnswer * divisor;
-                secondFactor = divisor;
-                symbol = "&divide;";
-            } else
-                window.alert("Unknown ?operation");
+            /* MATH CORE END */
             document.querySelector("#question-span").innerHTML = "" + firstFactor + " " + symbol + " " + secondFactor;
             var incorrectAnswers = [];
             for(var i = 0; i < (NUM_COLUMNS-1); i++) {
